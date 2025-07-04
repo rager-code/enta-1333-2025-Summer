a484c7957b2f12840794b4eb62ec01d70d65f742
rager-code <nuclearshock256@gmail.com>
2025-07-03 21:25:29 -0700

    Sounds

---
+++ 1333_Template/Assets/Audio/SoundPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource m_MyAudioSourcee;
+
+    public void Update()
+    {
+        m_MyAudioSource.Play();
+
+    }
+
+    public void playSound()
+    {
+       
+
+
+    }
+}
